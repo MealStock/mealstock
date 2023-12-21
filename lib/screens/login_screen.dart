f98ae2bd79ada0eaccffd0b5a3aa78dafa6595ac
czmatejt9 <105561491+czmatejt9@users.noreply.github.com>
2023-12-21 18:27:14 +0100

    lol sokrij yb tniopnug ta mai

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome',
+              style: TextStyle(
+                fontSize: 57,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Padding(padding: EdgeInsets.all(16.0)),
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 100.0),
+                child: const Placeholder(
+                    fallbackHeight: 100.0)), // logo placeholder
+            const Padding(padding: EdgeInsets.all(16.0)),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Sign up',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: Size(width * 0.8, 50.0)),
+            ),
+            const Padding(padding: EdgeInsets.only(top: 29)),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Login',
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  minimumSize: Size(width * 0.8, 50.0)),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
